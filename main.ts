9c0fcf17752a50584d6b8189ef01ebff3543ed30
Romain <romain.bertolini@gmail.com>
2024-10-11 05:28:41 +0000

    plugin rename and code cleanup

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, SuggestModal} from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, SuggestModal} from 'obsidian';
 import MovieDB = require('node-themoviedb');
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
+interface MovieLinkSettings {
 	tmdb_api_key: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
+const DEFAULT_SETTINGS: MovieLinkSettings = {
 	tmdb_api_key: ''
 }
 
@@ -52,11 +51,9 @@ class Movie{
 }
 
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class MovieLink extends Plugin {
+	settings: MovieLinkSettings;
 	mdb: MovieDB;
-
-
 
 	async getMovies(title: string) {
 		const result = new Array<Movie>();
@@ -78,12 +75,7 @@ export default class MyPlugin extends Plugin {
 
 
 			console.log(result);
-			/*
-			  {
-				data: Object. Parsed json data of response
-				headers: Object. Headers of response
-			  }
-			*/
+
 			return result;
 		} catch (error) {
 			console.error(error);
@@ -95,35 +87,11 @@ export default class MyPlugin extends Plugin {
 
 		await this.loadSettings();
 
-		// ES6 Style
-		// import MovieDB from 'node-themoviedb';
-		
 		this.mdb = new MovieDB(this.settings.tmdb_api_key, {});
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'insert movie based on title',
+			id: 'movielink-selectmovie-command',
+			name: 'insert movie link based on title',
 			editorCallback: async (editor: Editor, view: MarkdownView) => { 
 				const sel = editor.getSelection();
 				const movies = await this.getMovies(sel);
@@ -132,42 +100,14 @@ export default class MyPlugin extends Plugin {
 					return;
 				}
 
-				new ExampleModal(this.app, movies, editor, this.mdb).open();
+				new SuggestMovie(this.app, movies, editor, this.mdb).open();
 
 				console.log(`You have selected: ${sel}`);
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.addSettingTab(new MovieLinkSettingTab(this.app, this));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -183,7 +123,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-export class ExampleModal extends SuggestModal<Movie> {
+export class SuggestMovie extends SuggestModal<Movie> {
 
 	movies:Array<Movie>
 	editor: Editor
@@ -194,10 +134,8 @@ export class ExampleModal extends SuggestModal<Movie> {
 		this.editor = editor;
 		this.movies = movies;
 		this.mdb = mdb;
-		// for (const m of data.results) {
-		// 	this.movies.push({title: m.title, year: m.release_date, genre: String(m.genre_ids[0])})
-		// }
 	}
+
 	// Returns all available suggestions.
 	getSuggestions(query: string): Movie[] {
 		return this.movies.filter((movie) =>
@@ -227,26 +165,11 @@ export class ExampleModal extends SuggestModal<Movie> {
 	}
   }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText('Woah!');
-	}
+class MovieLinkSettingTab extends PluginSettingTab {
+	plugin: MovieLink;
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: MovieLink) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
