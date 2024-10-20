7d1235806cce14e9a190ea8faa15448dedf34339
Romain <romain.bertolini@gmail.com>
2024-10-20 05:35:36 +0000

    Corrected tvshow research, and small improvments

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting, SuggestModal} from 'obsidian';
 import MovieDB = require('node-themoviedb');
-import { time, timeEnd } from 'console';
 
 
 interface MovieLinkSettings {
@@ -15,8 +14,18 @@ class Media{
 	title: string;
 	id: number;
 	genres: Array<string>;
-	release: string;
+	release: Date;
 	link: string;
+	isTvShow: boolean;
+
+	public genresString() {
+		let str_genres = ""
+		for (const g of this.genres) {
+			str_genres += g;
+			str_genres += " / "
+		}
+		return str_genres.slice(0, -3);
+	}
 
 	public static fromMovieDetail (details_data: MovieDB.Responses.Movie.GetDetails): Media {
 		const cls = new Media();
@@ -28,7 +37,8 @@ class Media{
 			console.log(genre.name);
 			cls.genres.push(genre.name);
 		}
-		cls.release = details_data.release_date;
+		cls.release = new Date(details_data.release_date);
+		cls.isTvShow = false;
 		return cls;
 	}
 
@@ -41,26 +51,31 @@ class Media{
 			console.log(genre.name);
 			cls.genres.push(genre.name);
 		}
-		cls.release = details_data.first_air_date;
+		cls.release = new Date(details_data.first_air_date);
+		cls.isTvShow = true;
 		return cls;
 	}
 
 	get_year_of_release() {
-		const index = this.release.indexOf("-");
-		return this.release.substring(0, index);
+		return this.release.getFullYear();
 	}
 
-	async getLink(mdb: MovieDB) {
-		try{
+	async getImdbLink(mdb: MovieDB) {
+		if (this.isTvShow == false) {
+		// try{
 			const result = await mdb.movie.getExternalIDs({
 				pathParameters: {
 					movie_id: this.id
 				}
 			});
-			return result.data.imdb_id;
-		} catch (error) {
-			console.error(error);
-			console.error("Could not get external id for id" + String(this.id))
+			return "https://www.imdb.com/title/" + String(result.data.imdb_id);
+		} else {
+			const result = await mdb.tv.getExternalIDs({
+				pathParameters: {
+					tv_id: this.id
+				}
+			});
+			return "https://www.imdb.com/title/" + String(result.data.imdb_id);
 		}
 		return ""
 	}
@@ -72,23 +87,18 @@ export default class MovieLink extends Plugin {
 	settings: MovieLinkSettings;
 	mdb: MovieDB;
 
-	async getTvShows(title: string) {
+	public static async getTvShows(title: string, mdb: MovieDB) {
+		const args = {
+			query: {query: title}
+		};
+		const tvshows = await mdb.search.TVShows(args);
 
-		try {
-			const args = {
-				query: {query: title}
-			};
-			const tvshows = await this.mdb.search.TVShows(args);
+		const prom = Promise.all(tvshows.data.results.map(elt => MovieLink.getTvShowDetail(elt, mdb)));
 
-			const prom = Promise.all(tvshows.data.results.map(elt => MovieLink.getTvShowDetail(elt, this.mdb)));
-
-			return prom;
-		} catch (error) {
-			console.error(error);
-		}
+		return prom;
 	}
 
-	public static async getTvShowDetail(tvshow: MovieDB.Objects.TVShow, mdb: MovieDB) {
+	static async getTvShowDetail(tvshow: MovieDB.Objects.TVShow, mdb: MovieDB) {
 		const tvshow_details = mdb.tv.getDetails({
 			pathParameters: {
 				tv_id: tvshow.id
@@ -99,7 +109,7 @@ export default class MovieLink extends Plugin {
 
 	}
 
-	public static async getMovieDetail(movie: MovieDB.Objects.Movie, mdb: MovieDB) {
+	static async getMovieDetail(movie: MovieDB.Objects.Movie, mdb: MovieDB) {
 		const movie_details =  mdb.movie.getDetails({
 			pathParameters: {
 				movie_id: movie.id
@@ -110,23 +120,16 @@ export default class MovieLink extends Plugin {
 	}
 
 	public static async getMovies(title: string, mdb: MovieDB) {
-		try {
-			const args = {
-				query: {query: title}
-			};
+		const args = {
+			query: {query: title}
+		};
 
-			console.time("movie");
+		const movies = await mdb.search.movies(args);
 
-			const movies = await mdb.search.movies(args);
+		const prom = Promise.all(movies.data.results.map(elt => MovieLink.getMovieDetail(elt, mdb)));
 
-			const prom = Promise.all(movies.data.results.map(elt => MovieLink.getMovieDetail(elt, mdb)));
+		return prom;
 
-			console.timeEnd("movie");
-
-			return prom;
-		} catch (error) {
-			console.error(error);
-		}
 	}
 
 
@@ -142,7 +145,7 @@ export default class MovieLink extends Plugin {
 			editorCallback: async (editor: Editor, view: MarkdownView) => { 
 				const sel = editor.getSelection();
 
-				new SuggestMovie(this.app, editor, sel, this.mdb).open();
+				new SuggestMedia(this.app, editor, sel, this.mdb, true).open();
 
 				console.log(`You have selected: ${sel}`);
 			}
@@ -154,7 +157,7 @@ export default class MovieLink extends Plugin {
 			editorCallback: async (editor: Editor, view: MarkdownView) => { 
 				const sel = editor.getSelection();
 
-				new SuggestMovie(this.app, editor, sel, this.mdb).open();
+				new SuggestMedia(this.app, editor, sel, this.mdb, false).open();
 
 				console.log(`You have selected: ${sel}`);
 			}
@@ -164,7 +167,7 @@ export default class MovieLink extends Plugin {
 			id: 'movielink-insertmovie-command',
 			name: 'Insert movie link',
 			editorCallback: async (editor: Editor, view: MarkdownView) => { 
-				new SuggestMovie(this.app, editor, "", this.mdb).open();
+				new SuggestMedia(this.app, editor, "", this.mdb, true).open();
 			}
 		});
 
@@ -172,7 +175,7 @@ export default class MovieLink extends Plugin {
 			id: 'movielink-inserttvshow-command',
 			name: 'Insert TV-show link',
 			editorCallback: async (editor: Editor, view: MarkdownView) => { 
-				new SuggestMovie(this.app, editor, "", this.mdb).open();
+				new SuggestMedia(this.app, editor, "", this.mdb, false).open();
 			}
 		});
 
@@ -194,7 +197,7 @@ export default class MovieLink extends Plugin {
 	}
 }
 
-export class SuggestMovie extends SuggestModal<Media> {
+export class SuggestMedia extends SuggestModal<Media> {
 
 	medias:Array<Media>
 	sel: string
@@ -203,17 +206,21 @@ export class SuggestMovie extends SuggestModal<Media> {
 	lastSearch: string
 	lastSearchTime: number
 	private delayInMs = 250
+	suggestMovie = false
 
-	constructor(app: App, editor: Editor, sel: string, mdb: MovieDB) {
+	constructor(app: App, editor: Editor, sel: string, mdb: MovieDB, suggestMovie: boolean) {
 		super(app);
 		this.sel = sel;
 		this.editor = editor;
 		this.mdb = mdb;
+		this.suggestMovie = suggestMovie;
 	}
 
 	async getSuggestions(query: string): Promise <Media []> {
-
-		if (query === this.lastSearch || query.length < 3) return;
+		if (this.sel != "") {
+			query = this.sel;
+		}
+		if (query === this.lastSearch || query.length < 3) return [];
         const timestamp = Date.now();
         this.lastSearchTime = timestamp;
         const Sleep = (ms: number) =>
@@ -221,37 +228,47 @@ export class SuggestMovie extends SuggestModal<Media> {
         await Sleep(this.delayInMs);
         if (this.lastSearchTime != timestamp) {
             // Search is canceled by a newer search
-            return;
+            return [];
         }
 		this.lastSearch = query;
 
 		console.time("movie");
-		if (this.sel != "") {
-			query = this.sel;
+		
+		
+		let prom = null;
+
+		try {
+			if (this.suggestMovie == true) {
+				prom = MovieLink.getMovies(query, this.mdb);
+			} else {
+				prom = MovieLink.getTvShows(query, this.mdb);
+			}
+		} catch (error) {
+			new Notice("Error trying to retreive media list" + error);
+			console.error(error);
+			prom = []
 		}
-
-		const prom = MovieLink.getMovies(query, this.mdb);
-
+		console.timeEnd("movie")
 		return prom;
 	}
   
 	renderSuggestion(media: Media, el: HTMLElement) {
 		el.createEl("div", { text: media.title });
-		let str_genres = media.get_year_of_release() + " - "
-		for (const g of media.genres) {
-			console.log(str_genres);
-			str_genres += g;
-			str_genres += " / "
-		}
+		const str_genres = media.get_year_of_release() + " - " + media.genresString()
 		el.createEl("small", { text: str_genres });
 	}
 
 	async onChooseSuggestion(media: Media, evt: MouseEvent | KeyboardEvent) {
 		new Notice(`Selected ${media.title}`);
-		const id = await media.getLink(this.mdb);
-		console.info(id);
-		this.editor.replaceSelection(
-			"[" + media.title + "](https://www.imdb.com/title/" + String(id) + ")");
+		try{
+			const link = await media.getImdbLink(this.mdb);
+			console.info(link);
+			this.editor.replaceSelection(
+				"[" + media.title + " (" + media.get_year_of_release() + ")" + " - " + media.genresString() + "](" + link + ")");	
+		} catch (error) {
+			new Notice("Error trying to retreive media link" + error);
+			console.error(error);
+		}
 	}
   }
 
